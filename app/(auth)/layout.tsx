0f241c10f114a176bf968a032c20a417700bdc88
Brandon Hernández <isahecam@gmail.com>
2025-11-09 20:14:16 -0600

    Add footer to AuthLayout with copyright information

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,11 +4,19 @@ export default function AuthLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <main className="flex min-h-screen w-full flex-col items-center justify-center">
+    <main className="flex min-h-screen w-full flex-col items-center justify-center gap-y-6">
       <header>
         <h1 className="text-2xl font-bold">Autenticación con Next Auth</h1>
       </header>
       {children}
+      <footer>
+        <p className="text-sm text-gray-500">
+          © 2025 Autenticación con Next Auth by{" "}
+          <a href="https://github.com/isahecam" rel="noopener noreferrer" target="_blank">
+            Brandon Hernández
+          </a>
+        </p>
+      </footer>
     </main>
   );
 }
